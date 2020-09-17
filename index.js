1c66d8b50d83bd5aa07c27e4e0f52aad7ec0061f
Alethea Cary <aletheacary@gmail.com>
2020-09-17 06:43:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const append = push(Odie)
 const prepend = unshift(Odie)
-const removeLast(test) = pop(test)
-const removeFirst(test) = shift(test)
+const removeLast() = pop()
+const removeFirst() = shift()
